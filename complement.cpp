5a483b60a5785c436fd911f6482f049bbb362865
Carolina <143666734+carolcv04@users.noreply.github.com>
2023-09-20 21:34:32 -0700

    Add files via upload

---
+++ complement.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    //seeting types for variables
+    int DNA_length, i;
+    string DNA, s;
+
+    //setting the variable as the command line
+    s = argv[0];
+    DNA_length = s.length();
+
+    //iterates through each character to find its complement
+    for(i = 0; i < DNA_length; i++){
+
+        if (s[i] == 'A'){
+            DNA += "T";
+        }
+        else if (s[i] == 'T'){
+            DNA += "A";
+        }
+        else if(s[i] == 'G'){
+            DNA += "C";
+        }
+        else if(s[i] == 'C'){
+            DNA += "G";
+        }
+    }
+    //erases ./ from ./GTAC
+    s = s.erase(0,2);
+
+    //prints out the DAN string & complement
+    cout << "DNA STRING: " << s << endl;
+    cout << "DNA COMPLEMENT: " << DNA << endl;
+    return 0;
+}
